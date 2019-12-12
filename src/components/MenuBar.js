28b2a0409f5633607391444a285a9202fb5950c4
Peter Sampson <peter.sampson010@gmail.com>
2019-12-12 12:53:26 +0000

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const MenuBar = (props) => {
+const MenuBar = (whatever) => {
 
   /*
 
@@ -17,19 +17,19 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className={(props.mainboxState.selectedItem === "profile" ? "item active" : "item")} id="profile" onClick={event => props.toggleItem(event)}>
+      <a className={(whatever.mainboxState.selectedItem === "profile" ? "item active" : "item")} id="profile" onClick={event => whatever.toggleItem(event)}>
         <i className="user large icon" id="profile" />
       </a>
 
-      <a className={(props.mainboxState.selectedItem === "photo" ? "item active" : "item")} id="photo" onClick={event => props.toggleItem(event)}>
+      <a className={(whatever.mainboxState.selectedItem === "photo" ? "item active" : "item")} id="photo" onClick={event => whatever.toggleItem(event)}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className={(props.mainboxState.selectedItem === "cocktail" ? "item active" : "item")} id="cocktail" onClick={event => props.toggleItem(event)}>
+      <a className={(whatever.mainboxState.selectedItem === "cocktail" ? "item active" : "item")} id="cocktail" onClick={event => whatever.toggleItem(event)}>
         <i className="cocktail large icon" id="cocktail" />
       </a>
 
-      <a className={(props.mainboxState.selectedItem === "pokemon" ? "item active" : "item")} id="pokemon" onClick={event => props.toggleItem(event)}> 
+      <a className={(whatever.mainboxState.selectedItem === "pokemon" ? "item active" : "item")} id="pokemon" onClick={event => whatever.toggleItem(event)}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
