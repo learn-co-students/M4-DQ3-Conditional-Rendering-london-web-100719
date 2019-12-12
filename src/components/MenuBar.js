8ea3a7358e065e0be5773e8b7878e330e7875a88
Liko <li_xie1992@hotmail.com>
2019-12-12 23:06:00 +0000

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,38 +1,52 @@
-import React from 'react'
+import React from "react";
 
-const MenuBar = (props) => {
-
+const MenuBar = props => {
   /*
 
   The 'a' tags below are the menu items. Think about the way a menu 
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
-  this programatically? What other behavior do we expect when we click
-  on a menu item? Do we need state in this component, and if not, how can
-  this component be made aware of what is currently the active menu item?
+  programatically? What other behavior do we expect when we click
+  on a menu item? Do we need state in component, and if not, how can
+  component be made aware of what is currently the active menu item?
 
   */
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
-        <i className="user large icon" id="profile"/>
+      <a
+        onClick={event => props.showItem(event.target.id)}
+        className={"item " + (props.active === "profile" ? "active" : null)}
+        id="profile"
+      >
+        <i className="user large icon" id="profile" />
       </a>
 
-      <a className="item" id="photo">
-        <i className="photo large icon" id="photo"/>
+      <a
+        onClick={event => props.showItem(event.target.id)}
+        className={"item " + (props.active === "photo" ? "active" : null)}
+        id="photo"
+      >
+        <i className="photo large icon" id="photo" />
       </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
+      <a
+        onClick={event => props.showItem(event.target.id)}
+        className={"item " + (props.active === "cocktail" ? "active" : null)}
+        id="cocktail"
+      >
+        <i className="cocktail large icon" id="cocktail" />
       </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
+      <a
+        onClick={event => props.showItem(event.target.id)}
+        className={"item " + (props.active === "pokemon" ? "active" : null)}
+        id="pokemon"
+      >
+        <i className=" themeisle large icon" id="pokemon" />
       </a>
     </div>
-  )
+  );
+};
 
-}
-
-export default MenuBar
+export default MenuBar;
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -1,28 +1,41 @@
-import React from 'react'
-import MenuBar from '../components/MenuBar.js'
-import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
+import React from "react";
+import MenuBar from "../components/MenuBar.js";
+import { Profile, Photos, Cocktails, Pokemon } from "../components/Pages.js";
 
 class MainBox extends React.Component {
+  state = {
+    active: "profile"
+  };
+  showItem = id => {
+    this.setState({ active: id });
+  };
 
-
+  detailsToDisplay = () => {
+    if (this.state.active === "profile") {
+      return <Profile />;
+    } else if (this.state.active === "photo") {
+      return <Photos />;
+    } else if (this.state.active === "cocktail") {
+      return <Cocktails />;
+    } else if (this.state.active === "pokemon") {
+      return <Pokemon />;
+    }
+    return null;
+  };
   render() {
-
     /*
 
     Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
-
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar active={this.state.active} showItem={this.showItem} />
+        {this.detailsToDisplay()}
       </div>
-    )
+    );
   }
-
 }
 
-export default MainBox
+export default MainBox;
